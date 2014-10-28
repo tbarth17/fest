b7ceb20fa00e67c4044cadbb3e37adc7c2520e7a
Tommy Barth <tommyhbrc@gmail.com>
2014-10-28 12:47:45 -0400

    Adds time to band page

--- app/js/controllers/bands_show_controller.js
+++ app/js/controllers/bands_show_controller.js
@@ -3,6 +3,10 @@ Fest.BandsShowController = Ember.ObjectController.extend({
   currentBand: Ember.computed.alias('model'),
 
   imgStyle: function(){
-  return new Ember.Handlebars.SafeString("background-image: url('"+this.get('bandImgUrl')+"')").toString();
-}.property('bandImgUrl')
+    return new Ember.Handlebars.SafeString("background-image: url('"+this.get('bandImgUrl')+"')").toString();
+}.property('bandImgUrl'),
+
+  bandStartTime: function(){
+    return moment(this.get('currentBand.bandStartTime')).zone('+0000').format('lll');
+  }.property('currentBand.bandStartTime')
 });
