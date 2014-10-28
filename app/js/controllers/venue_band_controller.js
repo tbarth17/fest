0fbae5b4357b78473eb806f42e8a0a70f3325931
Tommy Barth <tommyhbrc@gmail.com>
2014-10-28 12:37:01 -0400

    Finished dates and time

--- app/js/controllers/venue_band_controller.js
+++ app/js/controllers/venue_band_controller.js
@@ -1,5 +1,9 @@
 Fest.VenueBandController = Ember.ObjectController.extend({
   bandStartTime: function(){
     return moment(this.get('model.bandStartTime')).zone('+0000').format('lll');
-  }.property('model.bandStartTime')
+  }.property('model.bandStartTime'),
+
+  bandEndTime: function(){
+    return moment(this.get('model.bandEndTime')).zone('+0000').format('h:mm A');
+  }.property('model.bandEndTime')
 });
