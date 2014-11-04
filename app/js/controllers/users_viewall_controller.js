2f844a6a92197fc01d1a2d27118f30896c9f7481
Tommy Barth <tommyhbrc@gmail.com>
2014-11-04 17:12:07 -0500

    Adds ability to search users

--- app/js/controllers/users_viewall_controller.js
+++ app/js/controllers/users_viewall_controller.js
@@ -1,4 +1,14 @@
 Fest.UsersViewAllController = Ember.ArrayController.extend({
   sortAscending: true,
-  sortProperties: ['userName']
+  sortProperties: ['userName'],
+  filter: '',
+
+  filteredContent: function() {
+    var regexp = new RegExp(this.get('filter').toLowerCase());
+
+    return this.get('arrangedContent').filter(function(item) {
+      return regexp.test(item.get('userName').toLowerCase());
+    });
+
+  }.property('filter', 'model.userName')
 });
