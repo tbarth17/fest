0c7c9f209cb46eafae315afd8e3a56edccf50773
Tommy Barth <tommyhbrc@gmail.com>
2014-11-10 10:22:34 -0500

    Rebuilds

--- scripts/main.js
+++ scripts/main.js
@@ -426,6 +426,38 @@ Fest.VenueBandController = Ember.ObjectController.extend({
     return moment(this.get('model.bandEndTime')).zone('+0000').format('h:mm A');
   }.property('model.bandEndTime'),
 
+  // isSelected: function() {
+  //   var currentUser = this.get('controllers.session.currentUser');
+  //   var currentBand = this.get('model');
+  //   if (currentBand.get('bandAttendees').contains(currentUser)){
+  //     return true;
+  //   } else {
+  //     return false;
+  //   }
+  // }.property('model.bandAttendees.@each', 'controllers.session.currentUser'),
+  //
+  // timeSlotStyle: function() {
+  //   var start = this.get('model.bandStartTime');
+  //   var end = this.get('model.bandEndTime');
+  //   var bandBreak = this.get('model.bandBreak');
+  //   var timeSlot = Math.abs(end - start);
+  //   var ratio = timeSlot/30000;
+  //   var margin = bandBreak * 2;
+  //   return new Ember.Handlebars.SafeString("width:" +ratio+ "px; margin-left:" +margin+ "px;").toString();
+  // }.property('model.bandStartTime', 'model.bandEndTime', 'model.bandBreak')
+});
+
+Fest.ScheduleBandController = Ember.ObjectController.extend({
+  needs: ['session'],
+
+  bandStartTime: function(){
+    return moment(this.get('model.bandStartTime')).zone('+0000').format('h:mm A');
+  }.property('model.bandStartTime'),
+
+  bandEndTime: function(){
+    return moment(this.get('model.bandEndTime')).zone('+0000').format('h:mm A');
+  }.property('model.bandEndTime'),
+
   isSelected: function() {
     var currentUser = this.get('controllers.session.currentUser');
     var currentBand = this.get('model');
@@ -451,6 +483,10 @@ Fest.LoginController = Ember.Controller.extend(Ember.Validations.Mixin, {
   needs: ['session'],
   validations: Fest.User.Validations,
   flash: {},
+
+  imgStyle: function(){
+  return new Ember.Handlebars.SafeString("background-image: url('"+this.get('userImgUrl')+"')").toString();
+}.property('userImgUrl'),
 
   actions: {
     createUser: function(){
