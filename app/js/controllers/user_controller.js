d5c1b9b8e9a65b69dc50e4c58bc06f5156309ad5
Tommy Barth <tommyhbrc@gmail.com>
2014-11-10 12:26:14 -0500

    Adds full chat functionality

--- app/js/controllers/user_controller.js
+++ app/js/controllers/user_controller.js
@@ -1,7 +1,27 @@
 Fest.UserController = Ember.ObjectController.extend({
   needs: ['application', 'session'],
 
+  actions: {
+    addMessage: function() {
+      var currentUser = this.get('controllers.session.currentUser');
+      var viewedUser = this.get('model');
+      var message = this.store.createRecord('message', {
+        messageText: this.get('messageText'),
+        messageTime: new Date(),
+        postingUser: currentUser,
+        postedTo: viewedUser
+      });
+      currentUser.get('userPostedMessages').addObject(message);
+      viewedUser.get('userReceivedMessages').addObject(message);
+      message.save();
+      currentUser.save();
+      viewedUser.save();
+      this.set('messageText', '');
+    }
+  },
+
   imgStyle: function(){
   return new Ember.Handlebars.SafeString("background-image: url('"+this.get('userImgUrl')+"')").toString();
-}.property('userImgUrl')
+}.property('userImgUrl'),
+
 });
