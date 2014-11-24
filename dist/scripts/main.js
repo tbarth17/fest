7b13ffef3b059a28a9d812f5b2bef69081e24dfe
Tommy Barth <tommyhbrc@gmail.com>
2014-11-24 15:39:30 -0800

    Rebuilds

--- dist/scripts/main.js
+++ dist/scripts/main.js
@@ -701,6 +701,13 @@ LiquidFire.map(function() {
 
   this.transition(
     this.fromRoute('user'),
+    this.toRoute('venues.show'),
+    this.use('toLeft'),
+    this.reverse('toLeft')
+  );
+
+  this.transition(
+    this.fromRoute('user'),
     this.toRoute('index'),
     this.use('toRight'),
     this.reverse('toLeft')
